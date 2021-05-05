50cf30e5d1493200954309f8aeaae44ba2aeff4d
Melina Oldorf <oldormel@students.zhaw.ch>
2021-05-06 00:21:44 +0200

    Completed task 3

--- week2-rexercise.R
+++ week2-rexercise.R
@@ -68,3 +68,110 @@ Wildschwein <- Wildschwein %>%
 View(Wildschwein)
 #What speed unit do you get?
 #Answer: m/s
+
+##Task 3 Cross-scale movement analysis
+caro <- read_delim("caro60.csv",",")
+caro<-st_as_sf(caro, coords = c("E", "N"), crs = 2056, remove = FALSE)
+
+View(caro)
+
+caro3<- seq(1,200, by=3) #why doesn't this easier variant work as well? Tried it with length.out also, but always got an error message. :S  
+caro3 <- caro[seq(1,nrow(caro),by=3),]
+caro6 <- caro[seq(1,nrow(caro),by=6),]
+caro9<- caro[seq(1,nrow(caro),by=9),]
+
+nrow(caro)
+nrow(caro3)
+nrow(caro6)
+nrow(caro9)
+
+#caro
+#timelag
+caro<- caro%>%
+  mutate(timelag = as.integer(difftime(lead(DatetimeUTC),DatetimeUTC, units="sec")))
+#steplength
+caro<-caro%>%
+  mutate(steplength = sqrt((E- lead(E,1))^2 + (N -lead(N,1))^2))
+#speed
+caro<-caro%>%
+  mutate(speed=steplength/timelag)
+
+#caro3
+#timelag
+caro3<- caro3%>%
+  mutate(timelag = as.integer(difftime(lead(DatetimeUTC),DatetimeUTC, units="sec")))
+#steplength
+caro3<-caro3%>%
+  mutate(steplength = sqrt((E- lead(E,1))^2 + (N -lead(N,1))^2))
+#speed
+caro3<-caro3%>%
+  mutate(speed=steplength/timelag)
+
+#caro6
+#timelag
+caro6<- caro6%>%
+  mutate(timelag = as.integer(difftime(lead(DatetimeUTC),DatetimeUTC, units="sec")))
+#steplength
+caro6<-caro6%>%
+  mutate(steplength = sqrt((E- lead(E,1))^2 + (N -lead(N,1))^2))
+#speed
+caro6<-caro6%>%
+  mutate(speed=steplength/timelag)
+
+#caro9
+#timelag
+caro9<- caro9%>%
+  mutate(timelag = as.integer(difftime(lead(DatetimeUTC),DatetimeUTC, units="sec")))
+#steplength
+caro9<-caro9%>%
+  mutate(steplength = sqrt((E- lead(E,1))^2 + (N -lead(N,1))^2))
+#speed
+caro9<-caro9%>%
+  mutate(speed=steplength/timelag)
+
+#Vizualised line plots
+
+#Original with 3 minutes
+ggplot(data=caro,mapping=aes(x=E, y=N))+
+  geom_path(data=caro,mapping=aes(color="1 minute"))+
+  geom_path(data = caro3, mapping=aes(color="3 minutes"))+
+  geom_point(data=caro,mapping=aes(color="1 minute"))+
+  geom_point(data = caro3, mapping=aes(color="3 minutes"))+
+  theme_light()+
+  labs(color="Trajectory", title = "Comparing original- with 3 minutes-resampled data")+
+  theme(title=element_text(size=10))
+
+#Original with 6 minutes
+ggplot(data = caro,mapping=aes(x=E, y=N))+
+  geom_path(data= caro,mapping=aes(color="1 minute"))+
+  geom_path(data = caro6, mapping=aes(color="6 minutes"))+
+  geom_point(data=caro,mapping=aes(color="1 minute"))+
+  geom_point(data = caro6, mapping=aes(color="6 minutes"))+
+  theme_light()+
+  labs(color="Trajectory", title = "Comparing original- with 6 minutes-resampled data")+
+  theme(title=element_text(size=10))
+
+#Original with 9 minutes
+ggplot(data = caro,mapping=aes(x=E, y=N))+
+  geom_path(data= caro,mapping=aes(color="1 minute"))+
+  geom_path(data = caro9, mapping=aes(color="9 minutes"))+
+  geom_point(data=caro,mapping=aes(color="1 minute"))+
+  geom_point(data = caro9, mapping=aes(color="9 minutes"))+
+  theme_light()+
+  labs(color="Trajectory", title = "Comparing original- with 9 minutes-resampled data")+
+  theme(title=element_text(size=10))
+
+#Speed Comparison
+ggplot(data = caro,mapping=aes(x=DatetimeUTC, y=speed))+
+  geom_line(data=caro, mapping=aes(colour="1 minute"))+
+  geom_line(data=caro3, mapping=aes(colour="3 minutes"))+
+  geom_line(data=caro6, mapping=aes(colour="6 minutes"))+
+  geom_line(data=caro9, mapping=aes(colour="9 minutes"))+
+  theme_light()+
+  labs(color="Trajectory", title = "Comparing derived speed at different sampling intervals")+
+  theme(title=element_text(size=10))+
+  xlab("Time")+
+  ylab("Speed (m/s)")
+
+#The different lines for the different temporal granularities show a different amount of data, whereas the 1 minute trajectory covers a lot of Points and Speeding times, the amount of data decreases and 'tracking gaps' occur.
+
